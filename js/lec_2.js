83cf43b4127efb4d61155d08288259f3353f7189
eun4018 <tjdms3273@gmail.com>
2024-08-26 21:43:00 +0900

    드디어 성공

--- js/lec_2.js
+++ js/lec_2.js
@@ -1,23 +1,24 @@
-const Parent = document.getElementById("sub_lec_6")
+const Parent = document.getElementById("sub_lec_6");
 const Form = Parent.querySelector("form");
 const Input = Parent.querySelector("input");
-const List = Parent.querySelector(" ul");
-let todos = [];
 
+let todos = [];
 const Save = () => {
   localStorage.setItem("todos", JSON.stringify(todos));
   //JSON.stringify 자바스크립트 객체 -> 문자열로 변경
-}
+};
 
 const delItem = (event) => {
   //console.dir(event.target) // parentNode, parentElement파악 가능
   const Target = event.target.parentElement;
   // todos = todos.filter((todo) => todo.id != Target.id)
   // !=서로 형타입은 다르지만 비교 가능
-  todos = todos.filter((todo) => todo.id !== parseInt(Target.id)) // 문자열을 숫자로
+  todos = todos.filter((todo) => todo.id !== parseInt(Target.id)); // 문자열을 숫자로
   Save();
   Target.remove();
-}
+};
+const Lists = document.createElement("ul");
+Parent.appendChild(Lists);
 const addItem = (todo) => {
   if (todo.text !== "") {
     const child_li = document.createElement("li");
@@ -25,41 +26,42 @@ const addItem = (todo) => {
     const span = document.createElement("span");
 
     span.innerText = todo.text;
-    button.innerText = '❌';
-    button.addEventListener("click", delItem)
+    button.innerText = "❌";
+    button.addEventListener("click", delItem);
     // List.appendChild(child_li);
+    // Parent.appendChild(Lists);
     child_li.appendChild(span);
     child_li.appendChild(button);
-   Parent.appendChild(child_li);
-//     const para = document.createElement("p");
-// para.innerHTML = "This is a paragraph.";
-// document.getElementById("myDIV").appendChild(para);
+    Lists.appendChild(child_li);
+    //     const para = document.createElement("p");
+    // para.innerHTML = "This is a paragraph.";
+    // document.getElementById("myDIV").appendChild(para);
     child_li.id = todo.id;
   }
-}
+};
 const Handler = (event) => {
   event.preventDefault();
   const todo = {
     id: Date.now(),
-    text: Input.value
+    text: Input.value,
   };
   todos.push(todo);
   Save();
   addItem(todo);
-  Input.value = '';
-}
+  Input.value = "";
+};
 //기존에 저장된 할일이 있다면 불러오기
 const init = () => {
   // const userTodos = localStorage.getItem("todos");
   const userTodos = JSON.parse(localStorage.getItem("todos"));
-  console.log(userTodos)
+  console.log(userTodos);
   //객체 형태로 파싱
   if (userTodos) {
     userTodos.forEach((todo) => {
       addItem(todo);
-    })
+    });
     todos = userTodos;
   }
-}
-Form.addEventListener("submit", Handler)
+};
+Form.addEventListener("submit", Handler);
 init();
