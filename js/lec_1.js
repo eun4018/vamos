18c02ba3420a6d3009aedb5f0100757196ec9dbd
eun4018 <vkdnjdjq2403@gmail.com>
2024-08-26 11:08:49 +0900

    8월26일업데이트

--- js/lec_1.js
+++ js/lec_1.js
@@ -196,7 +196,68 @@ function lecture_05(){
     Math.random(Math.random() * 3)
 }
 
-
+function main_Lec_02(){
+  const video = document.querySelector("video");
+  const playButton = document.querySelector(".play-pause > span");
+  const rateButtons = document.querySelectorAll(".rate");
+  const volumeBar = document.querySelector("input");
+  const updateProgress = () => {
+    const percent = (video.currentTime / video.duration) * 100;
+    const progressBar = document.querySelector(".bar");
+    progressBar.style.width = `${percent}%`;
+  
+    if (video.ended) {
+      pause();
+    }
+  }
+  const formatting = (time) => {
+    const Sec = Math.floor(time % 60);
+    const Min = Math.floor(time / 60) % 60;
+    const Hour = Math.floor(time / 3600);
+  
+    const fsec = Sec < 10 ? `0${Sec}` : Sec;
+    const fmin = Min < 10 ? `0${Min}` : Min;
+    const fhour = Hour < 10 ? `0${Hour}` : Hour;
+  
+    return `${fhour}:${fmin}:${fsec}`;
+  }
+  const updateTime = () => {
+    const current = document.querySelector(".current");
+    const duration = document.querySelector(".duration"); // duration부분을 계속 업뎃필요없음
+    current.innerText = formatting(video.currentTime);
+    duration.innerText = formatting(video.duration);
+  }
+  const setVolume = (event) => {
+    //0부터 1까지의 값
+    video.volume = event.target.value;
+  }
+  const setRate = (event) => {
+    const { rate } = event.target.dataset; // 구조분해할당 ES6 
+    video.playbackRate = rate;
+  }
+  
+  const play = () => {
+    playButton.innerText = '||';
+    video.play();
+  }
+  const pause = () => {
+    playButton.innerText = '▶';
+    video.pause();
+  }
+  
+  const togglePlay = () => {
+    //삼항 연산자 사용
+    video.paused ? play() : pause();
+  }
+  playButton.addEventListener("click", togglePlay);
+  rateButtons.forEach((button) => {
+    button.addEventListener("click", setRate);
+  });
+  
+  volumeBar.addEventListener("change", setVolume);
+  video.addEventListener("timeupdate", updateTime);
+  video.addEventListener("timeupdate", updateProgress);
+}
 
 
 lecture_01();
@@ -204,3 +265,4 @@ lecture_02();
 lecture_03();
 lecture_04();
 lecture_05();
+main_Lec_02();
