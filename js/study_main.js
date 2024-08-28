68ab9d7880a9a4a6d105df2273ef8713f3aff5fa
eun4018 <tjdms3273@gmail.com>
2024-08-28 21:23:16 +0900

    결국이렇게

--- js/study_main.js
+++ js/study_main.js
@@ -4,9 +4,26 @@ let target_id;
 const lectures = document.querySelectorAll(".lecture");
 const lec2 = document.querySelectorAll(".sub");
 const popup = (event) => {
-   let target_id = event.target.hash;
-   document.querySelector(target_id).style.display = "flex";
- };
+  let target_id = event.target.hash;
+  document.querySelector(target_id).style.display = "flex";
+};
+const submenu = () => {
+  sub_target.forEach((subList) => {
+    lec2[0].style.display = "flex";
+    sub_target[0].classList.add("on");
+    subList.addEventListener("click", (event) => {
+      sub_target.forEach((subList) => {
+        subList.classList.remove("on");
+      });
+      subList.classList.add("on");
+      lec2.forEach((sub) => {
+        sub.style.display = "none";
+      });
+      popup(event);
+    });
+  });
+};
+submenu();
 target.forEach((list) => {
   lectures[0].style.display = "flex";
   target[0].classList.add("on");
@@ -17,22 +34,8 @@ target.forEach((list) => {
     list.classList.add("on");
     lectures.forEach((lecture) => {
       lecture.style.display = "none";
+      submenu();
     });
     popup(event);
   });
 });
-sub_target.forEach((subList)=>{
-   lec2[0].style.display = "flex";
-   sub_target[0].classList.add("on");
-   subList.addEventListener("click", (event) => {
-      sub_target.forEach((subList) => {
-         subList.classList.remove("on");
-       });
-       subList.classList.add("on");
-      lec2.forEach((sub) => {
-         sub.style.display = "none";
-       });
-      popup(event);
-   })
-})
-
