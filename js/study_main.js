eeb80dcdec2b16a7f50b586fe64288b43e300813
eun4018 <vkdnjdjq2403@gmail.com>
2024-09-02 17:52:40 +0900

    2409_메뉴들새롭게작업

--- js/study_main.js
+++ js/study_main.js
@@ -1,41 +1,52 @@
-const target = document.querySelectorAll(".list");
-const sub_target = document.querySelectorAll(".subList");
-let target_id;
-const lectures = document.querySelectorAll(".lecture");
-const lec2 = document.querySelectorAll(".sub");
-const popup = (event) => {
-  let target_id = event.target.hash;
-  document.querySelector(target_id).style.display = "flex";
-};
-const submenu = () => {
-  sub_target.forEach((subList) => {
-    lec2[0].style.display = "flex";
-    sub_target[0].classList.add("on");
-    subList.addEventListener("click", (event) => {
-      sub_target.forEach((subList) => {
-        subList.classList.remove("on");
-      });
-      subList.classList.add("on");
-      lec2.forEach((sub) => {
-        sub.style.display = "none";
-      });
-      popup(event);
-    });
-  });
-};
-submenu();
-target.forEach((list) => {
-  lectures[0].style.display = "flex";
-  target[0].classList.add("on");
-  list.addEventListener("click", (event) => {
+const submenu = (e) => {
+  const tab  = document.querySelector(".lecture");
+  const Parent = document.querySelector(".sub-menu")
+  const tabItem = Parent.querySelectorAll(" .subList");
+  const tabContent = document.querySelectorAll(".sub");
+tabItem[0].classList.add("active")
+tabContent[0].classList.add("show");
+ tabItem.forEach((subList) => {
+    subList.addEventListener("click",(e)=>{
+      tabItem.forEach((subList)=>{
+        subList.classList.remove("active")
+      })
+      subList.classList.add("active")
+        tabContent.forEach((sub) => {
+          const listOrder = e.target.dataset.list;
+            if( sub.dataset.order === listOrder){
+              sub.classList.add("show");
+            } else sub.classList.remove("show");
+        })
+    })
+  })
+}
+submenu()
+const Mainmenu_tab = () => {
+  const target = document.querySelectorAll(".list");
+    let target_id;
+    const lectures = document.querySelectorAll(".lecture");
+    const popup = (event) => {
+      let target_id = event.target.hash;
+      document.querySelector(target_id).style.display = "flex";
+    };
     target.forEach((list) => {
-      list.classList.remove("on");
+      lectures[0].style.display = "flex";
+      target[0].classList.add("on");
+      list.addEventListener("click", (event) => {
+        target.forEach((list) => {
+          list.classList.remove("on");
+        });
+        list.classList.add("on");
+        lectures.forEach((lecture) => {
+          lecture.style.display = "none";
+        });
+        popup(event);
+      });
     });
-    list.classList.add("on");
-    lectures.forEach((lecture) => {
-      lecture.style.display = "none";
-      submenu();
-    });
-    popup(event);
-  });
-});
+    // lectures.forEach((lecture) => {
+    //   submenu(lecture)
+    // });   
+
+}
+Mainmenu_tab(); 
+
